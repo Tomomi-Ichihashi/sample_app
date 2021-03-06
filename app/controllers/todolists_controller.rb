74e7466f1bd32fc83e99a941ac68abcde4fc91bf
EC2 Default User <ec2-user@ip-172-31-65-87.ec2.internal>
2021-03-06 09:03:10 +0000

    [Add]画像投稿機能

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,9 +1,10 @@
 class TodolistsController < ApplicationController
   def new
     # Viewへ渡すためのインスタンス変数にからのモデルオプジェクトを生成する
-    ＠list = List.new
+    @list = List.new
+
   end
-  
+
   # 以下を追加
   def create
     # 1.データを新規登録するためのインスタンス作成
@@ -14,31 +15,32 @@ class TodolistsController < ApplicationController
     # redirect_to '/top'を削除して、以下コードに変更
     # 詳細画面へリダイレクト
     redirect_to todolist_path(list.id)
-    
+
   end
-  
+
   def index
     @lists = List.all
   end
-  
+
   def show
     @list = List.find(params[:id])
   end
-  
+
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
-  
+
   private
   # ストロングパラメータ
   def list_params
-    params.permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
+
   end
-    
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7f71233e60f9d875c0aba3a6b6fd7bf069a10b6c5465a8cc8967dd7d7755f54977b386b9de2ee9d053f42dc9d16a214f47fc4c7eb1d5c0cb27b681cd0a82f104'
